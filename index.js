7d7e82904561d7ee27316de6bb840c547985e900
Aditya Moorjmalani <adityamoorjmalani@Adityas-MacBook-Air.local>
2026-02-01 23:09:05 +0530

    bug solved

--- index.js
+++ index.js
@@ -25,10 +25,17 @@ app.get("/students", async (req, res) => {
 });
 
 app.post("/students", async (req, res) => {
-  const { name, age, grade } = req.body;
+  const { name, age, grade, gender, marks, attendance } = req.body;
 
   try {
-    const student = new Student({ name, age, grade });
+    const student = new Student({
+      name,
+      age,
+      grade,
+      gender,
+      marks,
+      attendance,
+    });
     await student.save();
     res.status(201).json(student);
   } catch (error) {
